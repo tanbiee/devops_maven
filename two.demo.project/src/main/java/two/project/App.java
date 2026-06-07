74f9573c13a3526c4286e5492339063906c35cba
shalini <hey@gamil.com>
2026-06-07 14:48:14 +0530

    adding jenkins file with dockerfile to automate push

---
+++ two.demo.project/src/main/java/two/project/App.java
@@ -0,0 +1,16 @@
+package two.project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("this is the maven project");
+        function();
+    }
+
+    public static void function() {
+        System.out.println("this is a function");
+    }
+}
